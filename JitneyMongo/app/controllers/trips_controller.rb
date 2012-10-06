e7d7326f0f03e263ae273edb51fb8116268c09db
Jack Eisenberg <jack@goshi.me>
2012-10-06 13:33:43 -0500

    added bootstrap

--- JitneyMongo/app/controllers/trips_controller.rb
+++ JitneyMongo/app/controllers/trips_controller.rb
@@ -14,6 +14,7 @@ class TripsController < ApplicationController
   # GET /trips/1.json
   def show
     @trip = Trip.find(params[:id])
+    #@json = @trip.to_gmaps4rails
 
     respond_to do |format|
       format.html # show.html.erb
--- JitneyMongo/app/models/trip.rb
+++ JitneyMongo/app/models/trip.rb
@@ -1,7 +1,11 @@
 class Trip
   include Mongoid::Document
+  
   field :name, type: String
   field :start, type: DateTime
   field :loc, type: Array
   field :end, type: DateTime
+  
+  acts_as_gmappable :position => :loc
+  
 end
